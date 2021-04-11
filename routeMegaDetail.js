ab34a93e62c90b7c578314cf41927e472516f622
thecommeneagle <82296168+thecommeneagle@users.noreply.github.com>
2021-04-11 23:54:13 +0100

    Add files via upload

---
+++ routeMegaDetail.js
@@ -0,0 +1,10 @@
+function initMap () {
+
+	const routeHighlights = [
+		{"routeHighlight": "s1", "coords": {"lat": 51.541837, "lng": -0.139199}},
+		{"routeHighlight": "s2", "coords":  {"lat":51.550396, "lng": -0.140526}},
+		{"routeHighlight": "s3", "coords":  {"lat": , "lng":   }},
+		{"routeHighlight": "s4", "coords":  {"lat": , "lng":   }},
+		{"routeHighlight": "s5", "coords":  {"lat":, "lng":    }},
+		
+	];
